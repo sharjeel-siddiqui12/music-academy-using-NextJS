84e993bc7b27a6c54385dac544f3f59eab7da5b6
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2025-05-18 02:08:42 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Hello</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      {/* <h1 className="text-4xl md:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-center drop-shadow-lg py-8">
+        Welcome to Our Music School
+      </h1>
+      <p className="mt-4 text-lg text-white text-center max-w-xl mx-auto bg-white/10 rounded-lg p-4 shadow-lg">
+        Explore our courses and start your musical journey today!
+      </p> */}
+
+      <HeroSection />
+    </main>
+    
   );
 }
